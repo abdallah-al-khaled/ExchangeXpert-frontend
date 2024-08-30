9e361c3c86d1816f35ab9f7084469d9164c73a7c
Abdallah <fafafavd@gmail.com>
2024-08-30 11:26:32 +0300

    Added AI trading bots in the sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function Sidebar() {
-
-  let btn = document.querySelector('#btn');
-  let sidebar = document.querySelector('.sidebar');
+  let btn = document.querySelector("#btn");
+  let sidebar = document.querySelector(".sidebar");
 
   const [isOpen, setIsOpen] = useState(false);
   const toggleSidebar = () => {
@@ -14,22 +13,31 @@ function Sidebar() {
   };
 
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`} onClick={toggleSidebar}>
+    <div className={`sidebar ${isOpen ? "open" : ""}`} onClick={toggleSidebar}>
       <div className="top">
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
         <box-icon name="menu" id="btn"></box-icon>
-        </div>
+      </div>
 
       <ul>
-      <li>
+        <li>
           <Link to="/Markets">
             <box-icon name="stocks"></box-icon>
             <span className="nav-item">Markets</span>
           </Link>
           <span className="tooltip">Markets</span>
         </li>
+
+        <li>
+          <Link to="/AI-bots">
+            <box-icon name="bot"></box-icon>
+            <span className="nav-item">AI Trading Bots</span>
+          </Link>
+          <span className="tooltip">AI Trading Bots</span>
+        </li>
+
         
 
       </ul>
