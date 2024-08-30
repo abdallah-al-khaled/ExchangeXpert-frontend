eed50aa89d2ca784257b5f0964edf18c833b4f2b
Abdallah <fafafavd@gmail.com>
2024-08-30 17:27:03 +0300

    Added styles for the sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -15,50 +15,69 @@ function Sidebar() {
 
   return (
     <>
-    <div className={`sidebar ${isOpen ? "open" : ""}`} onClick={toggleSidebar}>
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <div className="top">
         <div className="logo">
           <img src={logo} alt="logo" />
+          <span>ExchangeXpert</span>
         </div>
-        <box-icon name="menu" id="btn"></box-icon>
+        <box-icon color="white" name="menu" id="btn" onClick={toggleSidebar}></box-icon>
       </div>
 
       <ul>
         <li>
-          <Link to="/Markets">
-            <box-icon color="white" name="stocks"></box-icon>
+          <Link to="/Markets" className="nav-link">
+            <div className="icon">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 19L10 2.71191" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M3 19V4M17 16V9.5M17 5V1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M0 8.99988H6V15.9999H0V8.99988ZM7 4.99988H13V11.9999H7V4.99988Z" fill="white"/>
+<path d="M14 3.99988H20V10.4999H14V3.99988Z" fill="white"/>
+</svg>
+            </div>
+
             <span className="nav-item">Markets</span>
           </Link>
           <span className="tooltip">Markets</span>
         </li>
         
         <li>
-          <Link to="/AI-bots">
+          <Link to="/AI-bots" className="nav-link">
+          <div className="icon">
             <box-icon color="white"  name="bot"></box-icon>
+          </div>
             <span className="nav-item">AI Trading Bots</span>
           </Link>
           <span className="tooltip">AI Trading Bots</span>
         </li>
 
         <li>
-          <Link to="/wallet">
+          <Link to="/wallet" className="nav-link">
+          <div className="icon">
             <box-icon color="white"  name="wallet"></box-icon>
+          </div>
             <span className="nav-item">Wallet</span>
           </Link>
           <span className="tooltip">Wallet</span>
         </li>
 
         <li>
-          <Link to="/Settings">
-            <box-icon color="white"  name="cog"></box-icon>
+          <Link to="/Settings" className="nav-link">
+          <div className="icon">
+            <box-icon color="white"  name="cog" ></box-icon>
+          </div>
+            
             <span className="nav-item">Settings</span>
           </Link>
           <span className="tooltip">Settings</span>
         </li>
 
         <li>
-          <Link to="/login">
+          <Link to="/login" className="nav-link">
+          <div className="icon">
             <box-icon color="white"  name="log-in"></box-icon>
+          </div>
+            
             <span className="nav-item">login</span>
           </Link>
           <span className="tooltip">login</span>
@@ -66,10 +85,11 @@ function Sidebar() {
 
       </ul>
     </div>
+
     <div className="main-content">
-      <div className="container">
+      <div className="">
         <h1>
-          wellcome
+          Code Commerce
         </h1>
       </div>
     </div>
