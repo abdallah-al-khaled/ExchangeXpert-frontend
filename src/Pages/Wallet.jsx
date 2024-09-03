68719584c6d7077ec4108d826b4e583d7208201e
Abdallah <fafafavd@gmail.com>
2024-09-03 11:11:28 +0300

    Added wallet topbar

--- src/Pages/Wallet.jsx
+++ src/Pages/Wallet.jsx
@@ -1,12 +1,28 @@
 import Topbar from "../components/Topbar";
 import TopContainer from "../components/TopContainer";
 import TopNav from "../components/TopNav";
+import TradeViewChart from "../components/TradeViewChart";
 
 function Wallet() {
   return (
     <div className="main-content">
       <TopNav />
-      <Topbar />
+      <div className="topbar-container">
+      <div className="Best-stocks">
+          <h2>Best Stocks</h2>
+        </div>
+      <div className="stock-list-top-bar ">
+        <Topbar symbol="NVDA"/>
+        <Topbar />
+        <Topbar />
+        <Topbar />
+        <Topbar />
+        <Topbar />
+        </div>
+        </div>
+      <div className="viewchart">
+       <TradeViewChart/>
+      </div>
     </div>
   )
 //           <Topbar/>
