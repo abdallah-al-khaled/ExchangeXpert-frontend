0d5e7fa42f014e0a71736073b1a16b5eefee82bb
Abdallah <fafafavd@gmail.com>
2024-09-03 12:38:12 +0300

    Added trending up stocks section

--- src/Pages/Wallet.jsx
+++ src/Pages/Wallet.jsx
@@ -1,6 +1,7 @@
 import AccountHistoryChart from "../components/AccountHistoryChart";
 import Topbar from "../components/Topbar";
 import TopContainer from "../components/TopContainer";
+import TopContainerListItem from "../components/TopContainerListItem";
 import TopNav from "../components/TopNav";
 import TradeViewChart from "../components/TradeViewChart";
 
@@ -21,8 +22,19 @@ function Wallet() {
         <Topbar />
         </div>
         </div>
-      <div className="viewchart">
+      <div className="viewchart flex row">
        <AccountHistoryChart/>
+       <div className="trending-up">
+       <div className="top-active stocks-container">
+          <p className="trending">Trending Up</p>
+
+          <TopContainerListItem />
+          <TopContainerListItem />
+          <TopContainerListItem />
+          <TopContainerListItem />
+          <TopContainerListItem />
+        </div>
+       </div>
       </div>
       
     </div>
