f83b4aee8608fa1a6279c482e67e8d9f260a24dc
Abdallah <fafafavd@gmail.com>
2024-09-10 12:12:31 +0300

    Added terms and services

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
     <div className="login-container">
       <div className="login-image">
         <img src={img} alt="Login" />
-        <div className="login-image-text flex column">
+        <div className="login-image-text">
           <h4>
             Navigate the Markets
             <br />
@@ -25,28 +25,22 @@ const Login = () => {
       </div>
       <div className="login-form">
         <h2>Login</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-          <p></p>
-        </form>
+        {/* <label htmlFor="email">Email</label> */}
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          required
+        />
+        {/* <label htmlFor="password">Password</label> */}
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+          required
+        />
+        <button type="submit">Login</button>
+        <p className="terms-and-services">By logging in, you agree to follow our terms of service</p>
       </div>
     </div>
   );
