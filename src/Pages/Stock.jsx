c869fa2f1bfa9a6074ec4b324a2711511289c67b
Abdallah <fafafavd@gmail.com>
2024-09-15 11:18:34 +0300

    Added positive negative and neutral sentiment

--- src/Pages/Stock.jsx
+++ src/Pages/Stock.jsx
@@ -6,35 +6,41 @@ import GaugeChart from "../components/GaugeChart";
 import "../assets/css/stockpage.css";
 import { useEffect } from "react";
 import axios from "axios";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 function Stock() {
   const { symbol } = useParams();
-  const [sentiment, setSentiment]  = useState({})
-  useEffect(()=>{
-    const request = async()=>{
-      const {data} = await axios.get(`http://127.0.0.1:8000/api/sentiment-analysis/${symbol}`)
-      console.log((parseFloat(data.sentiment_score) + 1)*50);
-      setSentiment((parseFloat(data.sentiment_score) + 1)*50)
-      return data
-    }
-    request()
-  },[])
+  const [sentiment, setSentiment] = useState({});
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://127.0.0.1:8000/api/sentiment-analysis/${symbol}`
+        );
+        console.log((parseFloat(data.sentiment_score) + 1) * 50);
+        setSentiment(Math.floor((parseFloat(data.sentiment_score) + 1) * 50));
+        return data;
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    request();
+  }, []);
   console.log(symbol);
   return (
     <div className="main-content">
       <TopNav />
       <div className="page">
         <div className="flex upper">
-            <div className="tradingviewchart">
+          <div className="tradingviewchart">
             <TradeViewChart symbol={symbol} />
-            </div>
+          </div>
 
           <div className="gauge-chart">
             <div className=""></div>
             <div className=""></div>
             <h3>Technical analysis for {symbol}</h3>
-            <GaugeChart value={sentiment}/>
-            <p className="sell">{sentiment > 50 ? "Sell" : "Buy"}</p>
+            <GaugeChart value={sentiment} />
+            <p className={sentiment > 70 ? "green" : sentiment < 30 ? "red" : "yellow"} id={"sentiment"}>{sentiment > 70 ? "Positve" : sentiment < 30 ? "Negative" : "Neutral"}</p>
             <div className=""></div>
             <p className="last-updated">last updated 22-8-2024</p>
           </div>
