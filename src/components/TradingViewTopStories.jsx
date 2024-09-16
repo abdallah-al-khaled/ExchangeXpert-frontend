5175077fb1c07f975dd5076029efdc39634d9027
Abdallah <fafafavd@gmail.com>
2024-09-16 18:06:04 +0300

    Added props for symbol news

--- src/components/TradingViewTopStories.jsx
+++ src/components/TradingViewTopStories.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-const TradingViewTopStories = () => {
+const TradingViewTopStories = ({ symbol = "AAPL" }) => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-timeline.js';
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-timeline.js";
     script.async = true;
     script.innerHTML = JSON.stringify({
-      "feedMode": "symbol",
-      "isTransparent": true,
-      "displayMode": "regular",
-      "width": "100%",
-      "height": "100%",
-      "colorTheme": "dark",
-      "locale": "en",
-      "symbol": "NASDAQ:AAPL"
+      feedMode: "symbol",
+      isTransparent: true,
+      displayMode: "regular",
+      width: "100%",
+      height: "100%",
+      colorTheme: "dark",
+      locale: "en",
+      symbol: symbol,
     });
-    
-    const widgetContainer = document.getElementById('tradingview-widget');
+
+    const widgetContainer = document.getElementById("tradingview-widget");
     widgetContainer.appendChild(script);
-    
+
     return () => {
       // Cleanup the widget on unmount
-      widgetContainer.innerHTML = '';
+      widgetContainer.innerHTML = "";
     };
   }, []);
 
   return (
     <div className="tradingview-widget-container">
-      <div id="tradingview-widget" className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-
-      </div>
+      <div
+        id="tradingview-widget"
+        className="tradingview-widget-container__widget"
+      ></div>
+      <div className="tradingview-widget-copyright"></div>
     </div>
   );
 };
