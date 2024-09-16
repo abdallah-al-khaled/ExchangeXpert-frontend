920cacef93103ec0b010256415a6c022e97019a5
Abdallah <fafafavd@gmail.com>
2024-09-16 18:05:00 +0300

    Added TradingView Top stories content

--- src/components/TradingViewTopStories.jsx
+++ src/components/TradingViewTopStories.jsx
@@ -26,7 +26,18 @@ const TradingViewTopStories = () => {
   }, []);
 
   return (
-    <div id="tradingview-widget"></div>
+    <div className="tradingview-widget-container">
+      <div id="tradingview-widget" className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+    </div>
   );
 };
 
