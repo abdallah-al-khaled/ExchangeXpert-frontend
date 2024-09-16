453d188f49daa1d36a4b6b257ac51f6a25181d3f
Abdallah <fafafavd@gmail.com>
2024-09-16 09:28:47 +0300

    Added api call to fetch worst stocks

--- src/features/sentimentSlice.js
+++ src/features/sentimentSlice.js
@@ -1,7 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchBestStocks = createAsyncThunk("sentiment/fetchBestStocks", async () => {
-    const response = await axios.get("http://127.0.0.1:8000/api/top-sentiment-stocks");
+export const fetchBestStocks = createAsyncThunk(
+  "sentiment/fetchBestStocks",
+  async () => {
+    const response = await axios.get(
+      "http://127.0.0.1:8000/api/top-sentiment-stocks"
+    );
     return response.data;
-  });
+  }
+);
+
+export const fetchWorstStocks = createAsyncThunk(
+  "sentiment/fetchWorstStocks",
+  async () => {
+    const response = await axios.get(
+      "http://127.0.0.1:8000/api/worst-sentiment-stocks"
+    );
+    return response.data;
+  }
+);
