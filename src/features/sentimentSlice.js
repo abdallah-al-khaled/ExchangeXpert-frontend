aa8a787f099ca050c2569ede1f8138ec90ab8a3a
Abdallah <fafafavd@gmail.com>
2024-09-16 09:28:00 +0300

    Added api call to fetch best stocks

--- src/features/sentimentSlice.js
+++ src/features/sentimentSlice.js
@@ -1,2 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+export const fetchBestStocks = createAsyncThunk("sentiment/fetchBestStocks", async () => {
+    const response = await axios.get("http://127.0.0.1:8000/api/top-sentiment-stocks");
+    return response.data;
+  });
