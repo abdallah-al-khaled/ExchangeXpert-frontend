15e3a36eb42c83b32f41f8d1757feb7ec391e2f2
Abdallah <fafafavd@gmail.com>
2024-09-16 18:05:30 +0300

    Removed the tradingview link

--- src/components/TradingViewTopStories.jsx
+++ src/components/TradingViewTopStories.jsx
@@ -29,13 +29,7 @@ const TradingViewTopStories = () => {
     <div className="tradingview-widget-container">
       <div id="tradingview-widget" className="tradingview-widget-container__widget"></div>
       <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
+
       </div>
     </div>
   );
